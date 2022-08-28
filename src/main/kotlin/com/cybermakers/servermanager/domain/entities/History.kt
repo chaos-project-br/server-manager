1b52a9bf62ac0193f15f63dd3086b543016b6a54
ImOhashi <leonardoohashi.mkt@gmail.com>
2022-08-28 00:55:21 -0300

    fix: add id in entities

--- src/main/kotlin/com/cybermakers/servermanager/domain/entities/History.kt
+++ src/main/kotlin/com/cybermakers/servermanager/domain/entities/History.kt
@@ -1,6 +1,15 @@
 package com.cybermakers.servermanager.domain.entities
 
+import org.springframework.data.annotation.Id
+import org.springframework.data.mongodb.core.mapping.Document
+import java.util.UUID
+
+@Document("history")
 data class History(
+
+    @Id
+    val _id: UUID = UUID.randomUUID(),
+
     val name: String,
     val dics: Double,
     val ram: Double,
--- src/main/kotlin/com/cybermakers/servermanager/domain/entities/ServerInfo.kt
+++ src/main/kotlin/com/cybermakers/servermanager/domain/entities/ServerInfo.kt
@@ -1,6 +1,15 @@
 package com.cybermakers.servermanager.domain.entities
 
+import org.springframework.data.annotation.Id
+import org.springframework.data.mongodb.core.mapping.Document
+import java.util.UUID
+
+@Document("server_info")
 data class ServerInfo(
+
+    @Id
+    val _id: UUID? = UUID.randomUUID(),
+
     val name: String,
     val ip: String,
     val owner: List<String>? = null,
